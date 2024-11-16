4ba8e6c33c253fd56817a048b3023ea7df90b678
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-16 14:10:25 +0100

    task view/url

--- tms_app/urls.py
+++ tms_app/urls.py
@@ -1,5 +1,11 @@
 from . import views
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+
 
 urlpatterns = [
     #path('', views.index, name="index"),
--- tms_app/views.py
+++ tms_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task, Notification
+from .serializers import TaskSerializer, NotificationSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+
